b1a7b88f00df3960a898b03d42c88cf72fe99f7c
Melissa Draper <melissa@meldraweb.com>
2020-07-31 21:52:56 -0700

    initial commit

---
+++ Source/Treehuggers/Treehuggers/Treehuggers.cs
@@ -0,0 +1,140 @@
+﻿namespace Treehuggers
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Reflection;
+    using RimWorld;
+    using HarmonyLib;
+    using Verse;
+
+    [DefOf]
+    public static class MyDefOf
+    {
+        public static TraitDef Vegan;
+        public static TraitDef Vegetarian;
+        public static ThoughtDef AteMeatTreehugger;
+        public static ThoughtDef AteAnimalProductTreehugger;
+        public static ThingCategoryDef AnimalProductRaw = DefDatabase<ThingCategoryDef>.GetNamed("AnimalProductRaw");
+
+        static MyDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(MyDefOf));
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+        private static readonly Type patchType = typeof(HarmonyPatches);
+
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony(id: "rimworld.elkyelky.Treehuggers");
+            harmony.Patch(original: AccessTools.Method(type: typeof(FoodUtility), name: nameof(FoodUtility.ThoughtsFromIngesting)), prefix: null,
+                 postfix: new HarmonyMethod(methodType: patchType, methodName: nameof(AddTreehuggerRawFoodThoughts_Postfix)));
+
+            harmony.Patch(original: AccessTools.Method(type: typeof(FoodUtility), name: "AddIngestThoughtsFromIngredient"), prefix: null,
+                postfix: new HarmonyMethod(methodType: patchType, methodName: nameof(AddTreehuggerMealFoodThoughts_Postfix)));
+
+            harmony.PatchAll();
+        }
+
+        public static void AddTreehuggerRawFoodThoughts_Postfix(Pawn ingester, Thing foodSource, ThingDef foodDef, ref List<ThoughtDef> ___ingestThoughts)
+        {
+            if (foodDef.IsMeat
+            && ingester.RaceProps.Humanlike
+            && (ingester.story.traits.HasTrait(MyDefOf.Vegetarian) || ingester.story.traits.HasTrait(MyDefOf.Vegan)))
+            {
+                ___ingestThoughts.Add(MyDefOf.AteMeatTreehugger);
+            }
+            else if (foodDef.thingCategories.Contains(MyDefOf.AnimalProductRaw)
+            && ingester.RaceProps.Humanlike
+            && ingester.story.traits.HasTrait(MyDefOf.Vegan))
+            {
+                ___ingestThoughts.Add(MyDefOf.AteAnimalProductTreehugger);
+            }
+        }
+
+        public static void AddTreehuggerMealFoodThoughts_Postfix(ThingDef ingredient, Pawn ingester, List<ThoughtDef> ingestThoughts)
+        {
+            if (ingredient.IsMeat
+            && ingester.RaceProps.Humanlike
+            && (ingester.story.traits.HasTrait(MyDefOf.Vegetarian) || ingester.story.traits.HasTrait(MyDefOf.Vegan)))
+            {
+                ingestThoughts.Add(MyDefOf.AteMeatTreehugger);
+            }
+            else if (!ingredient.thingCategories.Contains(MyDefOf.AnimalProductRaw)
+            && ingester.RaceProps.Humanlike
+            && ingester.story.traits.HasTrait(MyDefOf.Vegan))
+            {
+                ingestThoughts.Add(MyDefOf.AteAnimalProductTreehugger);
+            }
+        }
+    }
+
+    public class ThoughtWorker_AnyWoolApparel : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {       
+
+            string text = null;
+            int num = 0;
+            List<Apparel> wornApparel = p.apparel.WornApparel;
+            for (int i = 0; i < wornApparel.Count; i++)
+            {
+                if (wornApparel[i].Stuff.defName.Contains("Wool"))
+                //if (wornApparel[i].Stuff.)
+                {
+                    if (text == null)
+                    {
+                        text = wornApparel[i].def.label;
+                    }
+                    num++;
+                }
+            }
+            if (num == 0)
+            {
+                return ThoughtState.Inactive;
+            }
+            if (num >= 5)
+            {
+                return ThoughtState.ActiveAtStage(4, text);
+            }
+            return ThoughtState.ActiveAtStage(num - 1, text);
+        }
+    }
+
+    public class ThoughtWorker_AnyLeatherApparel : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            string text = null;
+            int num = 0;
+            List<Apparel> wornApparel = p.apparel.WornApparel;
+            for (int i = 0; i < wornApparel.Count; i++)
+            {
+                if (wornApparel[i].Stuff.IsLeather)
+                {
+                    if (text == null)
+                    {
+                        text = wornApparel[i].def.label;
+                    }
+                    num++;
+                }
+            }
+            if (num == 0)
+            {
+                return ThoughtState.Inactive;
+            }
+            if (num >= 5)
+            {
+                return ThoughtState.ActiveAtStage(4, text);
+            }
+            return ThoughtState.ActiveAtStage(num - 1, text);
+        }
+    }
+}
